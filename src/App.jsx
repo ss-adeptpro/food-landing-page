ac4e81d5611f3d6ec2e94bcd2a91a1f76ac4e8db
Sandy <sk.ajaxian@gmail.com>
2023-12-05 12:50:35 +1100

    Gallery feature

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery from "./components/Gallery/Gallery";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="siteContainer">
+      <Navbar></Navbar>
+      <Gallery></Gallery>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HearderGallery = () => {
+  return (
+    <div className='header-gallery'>
+      <div className="header-gllery-text">
+        <p>
+          <span className='px-1'>the</span>
+          <span className='px-1 text-fOrange-400'>best</span></p>
+        <p>
+          <span className='px-1 text-fOrange-400'>foods</span>
+          <span className='px-1'>delivered</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HearderGallery;
